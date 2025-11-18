fd545ee0c05d73911db6ff91c7f62a7431e8466d
onish-cmd <136346254+onish-cmd@users.noreply.github.com>
2025-11-18 21:54:36 +0530

    Update omni_YTX705.mk

--- omni_YTX705.mk
+++ omni_YTX705.mk
@@ -12,18 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from YT-X705X device
-$(call inherit-product, device/lenovo/YT-X705X/device.mk)
+# Inherit from YTX705 device (FIXED: Folder name)
+$(call inherit-product, device/lenovo/YTX705/device.mk)
 
-PRODUCT_DEVICE := YT-X705X
-PRODUCT_NAME := omni_YT-X705X
+PRODUCT_DEVICE := YTX705                       # FIXED: Product device name
+PRODUCT_NAME := omni_YTX705                    # FIXED: Product name
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := Lenovo YT-X705X
+PRODUCT_MODEL := Lenovo YTX705                 # FIXED: Product model
 PRODUCT_MANUFACTURER := lenovo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo-rev2
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="YT-X705X-user 10 QKQ1.191224.003 X705X_S001137_220802_ROW release-keys"
+    PRIVATE_BUILD_DESC="YTX705-user 10 QKQ1.191224.003 X705X_S001137_220802_ROW release-keys"  # FIXED: Build prop
 
-BUILD_FINGERPRINT := Lenovo/YT-X705X/YT-X705X:10/QKQ1.191224.003/X705X_S001137_220802_ROW:user/release-keys
+BUILD_FINGERPRINT := Lenovo/YTX705/YTX705:10/QKQ1.191224.003/X705X_S001137_220802_ROW:user/release-keys # FIXED: Fingerprint
