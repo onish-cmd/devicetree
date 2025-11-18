ec8279cd815ca925016823d3ead0a974f72fa6b3
onish-cmd <136346254+onish-cmd@users.noreply.github.com>
2025-11-18 21:55:13 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/lenovo/YT-X705X
+DEVICE_PATH := device/lenovo/YTX705
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
